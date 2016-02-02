a933f006c61f71285fcfd66ab3c9b8565138c457
Li-Yi Lin <jasonlingo@gmail.com>
2016-02-02 16:10:51 -0500

    fix q4 bug

--- handout/dbsys_hw0/sql/q4.todo.sql
+++ handout/dbsys_hw0/sql/q4.todo.sql
@@ -9,20 +9,17 @@
 -- Student SQL code here:
 
 
--- top 5 nations with the greatest value
--- (select c_nationkey as t_nationkey, n_name as t_name, sum(o_totalprice) as t_totalprice
--- from orders, customer, nation
--- where o_custkey = c_custkey and c_nationkey = n_nationkey
--- group by c_nationkey, n_name
--- order by t_totalprice desc limit 5)
 WITH topFiveBuyer AS
+-- for all the top five customer nations, sum over the all price groupped by the suppliers
 (select cn.n_name as custName, sn.n_name as suppName, sum(o_totalprice) as supp_totalprice
  from orders, lineitem, supplier, customer, partsupp, nation as cn, nation as sn,
+    -- find the top 5 customer nations
     (select c_nationkey as t_nationkey, n_name as t_name, sum(o_totalprice) as t_totalprice
        from orders, customer, nation
       where o_custkey = c_custkey and c_nationkey = n_nationkey
    group by c_nationkey, n_name
    order by t_totalprice desc limit 5) as topCust
+
  where o_orderkey = l_orderkey and o_custkey = c_custkey and t_nationkey = c_nationkey
    and l_partkey = ps_partkey and l_suppkey = ps_suppkey and ps_suppkey = s_suppkey
    and cn.n_nationkey = c_nationkey and sn.n_nationkey = s_nationkey
@@ -33,8 +30,8 @@ select custName, suppName, supp_totalprice
  from topFiveBuyer
 where (
     select count(*) from topFiveBuyer as t
-    where t.custName = topFiveBuyer.custName and t.supp_totalprice <= topFiveBuyer.supp_totalprice
+    where t.custName = topFiveBuyer.custName and t.supp_totalprice >= topFiveBuyer.supp_totalprice
 ) <= 5
-order by custName, suppName
- ;
+order by custName, supp_totalprice desc
+;
  
