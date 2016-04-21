65ef99870ef3f4c59170fa15fe6d887cec048741
Jinyi <jguo32@jhu.edu>
2016-04-21 15:51:06 -0400

    hw4 added

---
+++ dbsys-hw4/q6.sql
@@ -0,0 +1,15 @@
+set termout on;
+set autotrace traceonly;
+set timing on;
+
+SELECT 
+ sum(l_extendedprice * l_discount) as revenue
+FROM        
+ lineitem
+WHERE        
+ l_shipdate >= date '1994-01-01'
+ and l_shipdate < date '1995-01-01'
+ and l_discount between 0.06 - 0.01 and 0.06 + 0.01
+ and l_quantity < 24;
+
+quit;
